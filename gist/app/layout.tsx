3158ec429317b901f72f991f27861e9b34876bcd
Matthew Okadinya <matthewokadinya@gmail.com>
2024-05-16 09:30:22 +0100

    Import global css to add tailwind styles

--- gist/app/layout.tsx
+++ gist/app/layout.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from '@mui/material/styles';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import theme from '../../gist/app/theme';
+import type { Metadata } from 'next'
+import './globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
--- gist/app/page.tsx
+++ gist/app/page.tsx
@@ -1,6 +1,9 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import * as React from "react";
 
-export default function ButtonUsage() {
-  return <Button variant="contained">Hello world</Button>;
+export default function Header() {
+  return (
+    <div>
+      Gist
+    </div>
+  );
 }
